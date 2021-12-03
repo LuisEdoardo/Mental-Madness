239ebb0095cd6b15e95886dc68713ec92795d4c1
unknown <lmorales6@ucol.mx>
2021-12-02 19:30:09 -0600

    Mental-Madness
    Fixed

--- End/Label.gd
+++ End/Label.gd
@@ -1,5 +1,5 @@
 extends Label
 
 func _ready():
-	self.text = str("Coins ", PlayerScore.score, "/10\n", "You died ", PlayerScore.deaths, " times")
+	self.text = str("Coins ", PlayerScore.score, "/33\n", "You died ", PlayerScore.deaths, " times")
 
